058267a1cc1cbff7dba5f9696127af10ad7a6a37
owenmyoven <o2841077794@hotmail.com>
2021-07-31 11:04:19 -0700

    Cleanup
    Getting rid of all the the  unnessary stuff

--- game1.py
+++ game1.py
@@ -1,5 +1,4 @@
-### The person needs a starting point and a destination
-
+### Importing different modules
 import pygame
 from pygame.locals import *
 from random import randint,uniform
@@ -7,28 +6,31 @@ from math import sqrt, sin
 from time import daylight, time 
 import sys, os
 
+### Initialization of pygame
 pygame.init()
 
+### Setting the FPS and the clock
 FPS = 60
 clock = pygame.time.Clock()
+
+
 ### Colors  
 WHITE = (255,255,255)
 DARK_GRAY = (20,20,20)
+
+### Setting up the Pygame window
 WIDTH, HEIGHT = 1920, 1080
 DISPLAY_SURF = pygame.display.set_mode((WIDTH,HEIGHT))
 DISPLAY_SURF.fill(WHITE)
-pygame.display.set_caption("Elevator")
-SPEED = 5
+pygame.display.set_caption("Game")
 BACKGROUND_COLOR = WHITE
-
 background = pygame.Surface((WIDTH,HEIGHT))
-
 background.fill(BACKGROUND_COLOR)
 
 
 
 
-
+### Setting up the player
 
 class Person(pygame.sprite.Sprite):
     def __init__(self,x,y):
@@ -57,28 +59,15 @@ class Person(pygame.sprite.Sprite):
         if pressed_keys[K_RIGHT]:
                   self.rect.move_ip(5, 0)
 
-class Elevator():
-    def __init__(self,x,y,final_y):
-        ##super().__init__()
-        
-        self.x = x
-        self.y = y
 
-        self.surf = pygame.Surface((0,0))
-        self.surf.fill((255, 255, 0))
-        self.rect = self.surf.get_rect(center = (x, y))
-    
-    def update():
-        pygame.draw.rect(DISPLAY_SURF,DARK_GRAY,(200,150,100,50))
         
 
 people = []
-elevators = []
+
 
 for i in range(1):
   people.append(Person(300,900))
-for i in range(1):
-    elevators.append(Elevator(1100,900,100))
+
 
 
 while True:
@@ -94,15 +83,11 @@ while True:
       for person in people:
         DISPLAY_SURF.blit(person.image, person.rect)
         person.update() 
-      for elevator in elevators:
-        DISPLAY_SURF.blit(elevator.surf, elevator.rect)
-        elevator.update() 
-    
+      
     except:
       pass
 
     pygame.display.update()
     clock.tick(FPS)
 
-### Bruh
-### Second Bruh
+
